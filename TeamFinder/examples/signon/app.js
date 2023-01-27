f659ebdeb24c74f08ca8100793388f73aceb0da8
8UCK37 <aneekenduviking666@gmail.com>
2023-01-27 10:51:38 +0530

    minor changes in ejs

--- TeamFinder/examples/signon/app.js
+++ TeamFinder/examples/signon/app.js
@@ -74,13 +74,13 @@ app.get('/', function(req, res){
 app.get('/account', ensureAuthenticated, async function(req, res){
   
   const c = await axios.get(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${req.user.id}&include_appinfo=true&format=json`);
-  console.log(c)
+  console.log(c);
   var games= c.data.response.games;
   if (games==undefined || games==null){
     games=[]
   }
+ 
   //
-
   res.render('account', { user: req.user,data: games });
   
 });
