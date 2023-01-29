53d57a00c86ce96f84ab0d3a0f389f819b461568
Barnamoy <48668097+barnamoy@users.noreply.github.com>
2023-01-29 13:13:20 +0530

    add authguard

--- TeamFinderFrontEnd/src/app/app-routing.module.ts
+++ TeamFinderFrontEnd/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PrimaryHomePageComponent } from './primary-home-page/primary-home-page.component';
+import { AuthGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
-  { path: 'first-component', pathMatch: 'full', component: PrimaryHomePageComponent },
-  { path: '', pathMatch: 'full', component: LoginComponent }
+  { path: 'first-component', pathMatch: 'full', component: PrimaryHomePageComponent , canActivate: [AuthGuard] },
+  { path: 'login-page', pathMatch: 'full', component: LoginComponent },
+  { path: '**', pathMatch: 'full', component: LoginComponent ,canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- TeamFinderFrontEnd/src/app/login/user.service.ts
+++ TeamFinderFrontEnd/src/app/login/user.service.ts
@@ -4,13 +4,26 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { map, switchMap, tap } from 'rxjs/operators';
 import firebase from 'firebase/compat/app';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+interface User {
+  uid: string;
+  email: string;
+  displayName: string;
+  photoURL: string;
+  emailVerified: boolean;
+}
 
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class UserService {
+  userData: any;
   uid = this.auth.authState.pipe(
     map((authState) => {
       if (!authState) {
@@ -20,7 +33,8 @@ export class UserService {
       }
     })
   );
-  
+
+
 
   // test to see if already logged in
   // isLoggedIn: Observable<boolean> = this.uid.pipe(
@@ -34,11 +48,57 @@ export class UserService {
   //   })
   // );
   // test to see if user has specific permissions
-  constructor(private auth: AngularFireAuth, private db: AngularFireDatabase) {}
+  constructor(
+    public afs: AngularFirestore,
+    private auth: AngularFireAuth,
+    private db: AngularFireDatabase,
+    public router: Router,
+    ) {
+    this.auth.authState.subscribe((user) => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user')!);
+      } else {
+        localStorage.setItem('user', 'null');
+        JSON.parse(localStorage.getItem('user')!);
+      }
+    });
+  }
+  // SetUserData(user: any) {
+  //   const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+  //     `users/${user.uid}`
+  //   );
+  //   const userData: User = {
+  //     uid: user.uid,
+  //     email: user.email,
+  //     displayName: user.displayName,
+  //     photoURL: user.photoURL,
+  //     emailVerified: user.emailVerified,
+  //   };
+  //   return userRef.set(userData, {
+  //     merge: true,
+  //   });
+  // }
   login() {
-    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then((result) => {
+      // this.SetUserData(result.user);
+      this.auth.authState.subscribe((user) => {
+        if (user) {
+          this.router.navigate(['']);
+        }
+      });
+    })
+    .catch((error) => {
+      window.alert(error.message);
+    });
   }
   logout() {
     this.auth.signOut();
+  }
+
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    return user !== null && user.emailVerified !== false ? true : false;
   }
 }
