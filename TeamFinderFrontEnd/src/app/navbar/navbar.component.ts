549d3c0f0aef32e42f4bcc027208064e64178d3f
soumajitLTI <95064182+Ca4RNaGe@users.noreply.github.com>
2023-01-31 14:27:49 +0530

    nav bar fixes

--- TeamFinderFrontEnd/src/app/navbar/navbar.component.ts
+++ TeamFinderFrontEnd/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { UserService } from '../login/user.service';
 
@@ -8,10 +8,26 @@ import { UserService } from '../login/user.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  @ViewChild('toggleButton') toggleButton!: ElementRef;
+  @ViewChild('menu') menu!: ElementRef;
   public show:boolean=false;
   router: any;
 
-  constructor(public user: UserService ,private renderer: Renderer2 ,private auth: AngularFireAuth) { }
+  constructor(public user: UserService ,private renderer: Renderer2 ,private auth: AngularFireAuth) {
+    this.renderer.listen('window', 'click',(e:Event)=>{
+      /**
+       * Only run when toggleButton is not clicked
+       * If we don't check this, all clicks (even on the toggle button) gets into this
+       * section which in the result we might never see the menu open!
+       * And the menu itself is checked here, and it's where we check just outside of
+       * the menu and button the condition abbove must close the menu
+       */
+     if(e.target !== this.toggleButton.nativeElement && e.target!==this.menu.nativeElement){
+         this.show=false;
+     }
+ });
+   }
 
   public usr:any;
   public userparsed:any;
