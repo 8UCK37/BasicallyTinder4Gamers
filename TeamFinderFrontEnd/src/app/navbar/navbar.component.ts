1aa1ac5fb5b1a603086bbc49d7eda0db723446f5
Barnamoy <48668097+barnamoy@users.noreply.github.com>
2023-01-31 12:22:26 +0530

    fix broken code

--- TeamFinderFrontEnd/src/app/navbar/navbar.component.ts
+++ TeamFinderFrontEnd/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { UserService } from '../login/user.service';
 
 @Component({
@@ -7,17 +8,23 @@ import { UserService } from '../login/user.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  public show:boolean=true;
+  public show:boolean=false;
   router: any;
 
-  constructor(public user: UserService ,private renderer: Renderer2) { }
+  constructor(public user: UserService ,private renderer: Renderer2 ,private auth: AngularFireAuth) { }
 
   public usr:any;
   public userparsed:any;
   ngOnInit(): void {
-    this.show=false;
-    this.usr = localStorage.getItem('user');
-    this.userparsed=JSON.parse(this.usr);
+    // this.show=false;
+    // this.usr = localStorage.getItem('user');
+    // this.userparsed=JSON.parse(this.usr);
+    // console.log("logged in :" ,  this.user.isLoggedIn)
+    this.auth.authState.subscribe(user=>{
+      if(user) {
+        this.userparsed = user
+      }
+    })
   }
 
   toggleMenu() {
