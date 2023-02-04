18b8f10acb33acde24f178c4d3d5ad180b58a11e
8UCK37 <aneekenduviking666@gmail.com>
2023-02-04 17:25:26 +0530

    idk man

--- TeamFinderFrontEnd/src/app/navbar/navbar.component.ts
+++ TeamFinderFrontEnd/src/app/navbar/navbar.component.ts
@@ -25,7 +25,6 @@ export class NavbarComponent implements OnInit {
        * the menu and button the condition abbove must close the menu
        */
      if(e.target !== this.toggleButton.nativeElement && e.target!==this.menu.nativeElement){
-        console.log("if")
          this.show=false;
       }
     });
