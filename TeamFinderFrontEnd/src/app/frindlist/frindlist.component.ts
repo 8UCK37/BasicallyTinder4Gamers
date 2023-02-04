948c1a5428f04deef41fb6488b5ba1421f7b31cd
Zaraki31 <100763853+Zaraki31@users.noreply.github.com>
2023-02-04 20:41:49 +0530

    Baler VS code part 3

---
+++ TeamFinderFrontEnd/src/app/frindlist/frindlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frindlist',
+  templateUrl: './frindlist.component.html',
+  styleUrls: ['./frindlist.component.css']
+})
+export class FrindlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
