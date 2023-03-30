1099c210f97c9d6a0f25eb143c99b8d561c9c657
8UCK37 <aneekenduviking666@gmail.com>
2023-03-30 12:19:21 +0530

    changed states to actual indian states and centered the userinfo modal

--- TeamFinderFrontEnd/src/app/primary-home-page/login-userinfo/login-userinfo.component.ts
+++ TeamFinderFrontEnd/src/app/primary-home-page/login-userinfo/login-userinfo.component.ts
@@ -25,24 +25,45 @@ export class LoginUserinfoComponent implements OnInit {
   selected2?: string;
   genderSelect?: string;
   inpmodel:any;
-  states = [
-    'Alabama',
-    'Alaska',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Alabama',
-    'Alaska',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Alabama',
-    'Alaska',
-    'Arizona',
-    'Arkansas',
-    'California',
-
+  public indianStates: string[] = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal",
+    "Andaman and Nicobar Islands",
+    "Chandigarh",
+    "Dadra and Nagar Haveli and Daman and Diu",
+    "Delhi",
+    "Ladakh",
+    "Lakshadweep",
+    "Puducherry"
   ];
+  ;
+  public languages:String[] =["Bengali","Hindi","English"]
   constructor(private modalService: BsModalService , private  _elementRef : ElementRef,private auth: AngularFireAuth , private userService: UserService ) {
 
   }
@@ -80,12 +101,12 @@ export class LoginUserinfoComponent implements OnInit {
   countrySelect(index:number){
     // console.log(index)
     // console.log(this.states[index])
-    this.selected1 = this.states[index]
+    this.selected1 = this.indianStates[index]
     this.cardStyle2 = []
     this.cardStyle = []
   }
   userLanguageSelect(index : number){
-    this.selected2 = this.states[index]
+    this.selected2 = this.indianStates[index]
     this.cardStyle = []
     this.cardStyle2 = []
 
