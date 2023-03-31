06f47e094821125d207f2f2e69f85c81dc44ab45
8UCK37 <aneekenduviking666@gmail.com>
2023-03-31 18:43:26 +0530

    fixed almost everything

--- TeamFinderFrontEnd/src/app/post/post.component.ts
+++ TeamFinderFrontEnd/src/app/post/post.component.ts
@@ -64,25 +64,10 @@ export class PostComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.images=[{
-      itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria1.jpg',
-      thumbnailImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria1s.jpg',
-      alt: 'Description for Image 1',
-      title: 'Title 1'
-  },
-  {
-      itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria2.jpg',
-      thumbnailImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria2s.jpg',
-      alt: 'Description for Image 2',
-      title: 'Title 2'
-  },
-  {
-      itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria3.jpg',
-      thumbnailImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria3s.jpg',
-      alt: 'Description for Image 3',
-      title: 'Title 3'
-  }]
 
+    this.childPost.photoUrlArr.forEach((url: any) => {
+      this.images.push({imageSrcUrl:url})
+    });
     this.commentService.postsObj$.subscribe(posts => {
       this.postsByTag= posts;
     });
