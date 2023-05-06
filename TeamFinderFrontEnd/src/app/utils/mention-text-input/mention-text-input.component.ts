b0339cf64128f05a2e5a277cbe49173fefca539c
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2023-05-06 13:30:20 +0530

    mention frontend works css html needs to be fixed

--- TeamFinderFrontEnd/src/app/utils/mention-text-input/mention-text-input.component.ts
+++ TeamFinderFrontEnd/src/app/utils/mention-text-input/mention-text-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import axios from 'axios';
 import { QuillEditorComponent } from "ngx-quill";
 
 import "quill-mention";
@@ -9,6 +10,8 @@ import "quill-mention";
   styleUrls: ['./mention-text-input.component.css']
 })
 export class MentionTextInputComponent implements OnInit {
+  searchResults: any[] = [];
+
   name = "Angular ";
   @Output() editorChange:EventEmitter<any> =new EventEmitter();
   @ViewChild(QuillEditorComponent, { static: true })
@@ -38,32 +41,36 @@ export class MentionTextInputComponent implements OnInit {
       allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
       showDenotationChar: true,
       spaceAfterInsert: false,
-      
+
       source: (searchTerm:any, renderList:any) => {
-        const values = [
-          { id: 1, value: "Fredrik Sundqvist", age: 5 },
-          { id: 2, value: "Patrik Sjölin", age: 20 }
-        ];
+        const values = [];
 
-        
-        if (searchTerm.length === 0) {
-          renderList(values, searchTerm);
-        } else {
+          if (searchTerm.length != 0) {
           const matches:any = [];
-
-          values.forEach(entry => {
-            if (
-              entry.value.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
-            ) {
-              matches.push(entry);
+          // values.forEach(entry => {
+          //   if (
+          //     entry.value.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
+          //   ) {
+          //     matches.push(entry);
+          //   }
+          // });
+          let i=0
+          this.getUsers(searchTerm).then(data=>{
+            if(data.length>5){
+              data.splice(5)
             }
-          });
-          renderList(matches, searchTerm);
+            data.forEach((user: any) => {
+              matches.push({id:i,value:user.name,user:user})
+              i++;
+            });
+            renderList(matches, searchTerm);
+          })
+          console.log(matches)
         }
       },
       renderItem : (item:any, searchTerm:any) =>{
         console.log(item)
-        return `<img src='https://static.vecteezy.com/system/resources/previews/002/002/403/original/man-with-beard-avatar-character-isolated-icon-free-vector.jpg' height=30px><span>${item.value}</span>`;
+        return `<img src=${item?.user.profilePicture} height=30px><span>${item.value}</span>`;
       },
     }
   };
@@ -71,14 +78,14 @@ export class MentionTextInputComponent implements OnInit {
   content = "";
 
   constructor() {
-    
+
   }
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
-   
+
   }
 
- 
+
 
   update() {
     this.editorChange.emit(this.editor?.quillEditor.getContents());
@@ -99,5 +106,15 @@ export class MentionTextInputComponent implements OnInit {
     // console.log({ result });
   }
 
+  async getUsers(name:any){
+    let searchResults:any=[];
+    if(name?.length!=0){
+      await axios.post('searchFriend',{searchTerm: name}).then(res=>{
+        searchResults=res.data;
+      }).catch(err=>console.log(err))
+    }
+    console.log(searchResults)
+    return searchResults
+  }
 
 }
