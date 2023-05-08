0b43cd1e1708423ee56a91ae103734201ea09160
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2023-05-08 14:03:28 +0530

    upload and cancel upload buttons added

--- TeamFinderFrontEnd/src/app/settings/chat-settings/chat-settings.component.ts
+++ TeamFinderFrontEnd/src/app/settings/chat-settings/chat-settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chat-settings',
@@ -8,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class ChatSettingsComponent implements OnInit {
 
   constructor() { }
-
+  @ViewChild('image') input!:ElementRef;
+  @ViewChild('previewImageElement', { static: false }) previewImageElement!: ElementRef<HTMLImageElement>;
+  public formData:any;
   ngOnInit(): void {
   }
 
+  previewImage() {
+    const file = this.input.nativeElement.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.previewImageElement.nativeElement.src = reader.result as string;
+    }
+    reader.readAsDataURL(file);
+  }
+
+
+  uploadChatBackground(){
+    
+  }
 }
