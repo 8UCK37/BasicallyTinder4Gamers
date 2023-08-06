aea872968cc79a947519594566430af4037cec87
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2023-08-06 22:38:07 +0530

    fixed reject spam

--- TeamFinderFrontEnd/src/app/navbar/navbar.component.ts
+++ TeamFinderFrontEnd/src/app/navbar/navbar.component.ts
@@ -148,7 +148,7 @@ export class NavbarComponent implements  OnInit {
           axios.post('getUserInfo', { id: noti.sender }).then(res => {
             noti.profileurl = res.data[0].profilePicture;
             noti.userName = res.data[0].name;
-
+            
             if(noti.notiType =="frndReqAcc"){
               this.messageService.add({ severity: 'success', summary: 'Accepted', detail: noti.userName.toString()+' accepted your friend request' });
             }
