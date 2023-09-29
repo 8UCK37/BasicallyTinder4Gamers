f06cf69316c0e413a794e3a4afcc2e82cd57fd5d
barnamoy <barna.moy@gmail.com>
2023-09-30 00:52:12 +0530

    Fix empty home page for user with no friend.

--- TeamFinder/examples/signon/postHelper.js
+++ TeamFinder/examples/signon/postHelper.js
@@ -134,68 +134,131 @@ async function editPost(req, res, prisma){
   
 }
 
-
-async function getPost(req, res, prisma) {
-  console.log("get post for", req.user.user_id);
+async function getRecommandedPost(req,prisma){
+  const posts = await prisma.$queryRaw`
+  SELECT 
+    p.*,
+    t.tagNames,
+    u."name",
+    u."profilePicture",
+    a.type AS reactionType,
+    CASE WHEN a.type IS NULL THEN true ELSE false END AS noReaction,
+    r.likeCount,
+    r.hahaCount,
+    r.sadCount,
+    r.loveCount,
+    r.poopCount,
+    CASE 
+      WHEN p.shared IS NULL THEN NULL 
+      WHEN pp.deleted = true THEN '{"deleted":"true","message": "This post is no longer available"}'
+      ELSE row_to_json(pp)::text 
+    END AS ParentPost,
+    json_build_object('name', pu."name", 'profilePicture', pu."profilePicture") as parentPostAuthor,
+    COALESCE(c.commentCount, 0) AS commentCount,
+    COALESCE(s.sharedCount, 0) AS sharedCount -- Include the sharedCount for each post
+  FROM public."Posts" p
+  LEFT JOIN (
+    SELECT post,
+           COUNT(*) FILTER (WHERE type = 'like') AS likeCount,
+           COUNT(*) FILTER (WHERE type = 'haha') AS hahaCount,
+           COUNT(*) FILTER (WHERE type = 'sad') AS sadCount,
+           COUNT(*) FILTER (WHERE type = 'love') AS loveCount,
+           COUNT(*) FILTER (WHERE type = 'poop') AS poopCount
+    FROM public."Activity"
+    GROUP BY post
+  ) r ON r.post = p.id
+  LEFT JOIN (
+    SELECT post, STRING_AGG("tagName", ',') AS tagNames
+    FROM public."Tags"
+    GROUP BY "post"
+  ) t ON p.id = t.post
+  LEFT JOIN public."Activity" a ON p.id = a.post AND a.author = ${req.user.user_id}
+  LEFT JOIN public."User" u ON p.author = u.id
+  LEFT JOIN public."Posts" pp ON pp.id = p.shared
+  LEFT JOIN public."User" pu ON pp.author = pu.id
+  LEFT JOIN (
+    SELECT post_id, COUNT(*) as commentCount -- Subquery to get the comment count for each post
+    FROM public."Comment"
+    GROUP BY post_id
+  ) c ON p.id = c.post_id
+  LEFT JOIN (
+    SELECT shared, COUNT(*) as sharedCount -- Subquery to get the shared count for each post
+    FROM public."Posts"
+    GROUP BY shared
+  ) s ON p.id = s.shared
+  WHERE EXISTS (
+    SELECT 1
+    FROM public."Friends" f
+    WHERE f.sender = ${req.user.user_id} AND f.reciever = p.author
+  ) AND p.deleted = false
+  ORDER BY p."createdAt" DESC;
+`;
+  return posts;
+}
+async function getGenericPost(req,prisma){
   const posts = await prisma.$queryRaw`
     SELECT 
       p.*,
       t.tagNames,
-      u."name",
+      u.name,
       u."profilePicture",
-      a.type AS reactionType,
-      CASE WHEN a.type IS NULL THEN true ELSE false END AS noReaction,
-      r.likeCount,
-      r.hahaCount,
-      r.sadCount,
-      r.loveCount,
-      r.poopCount,
+      (
+        SELECT a.type
+        FROM public."Activity" a
+        WHERE a.author = ${req.user.user_id} AND a.post = p.id
+      ) AS reactionType,
       CASE 
         WHEN p.shared IS NULL THEN NULL 
         WHEN pp.deleted = true THEN '{"deleted":"true","message": "This post is no longer available"}'
         ELSE row_to_json(pp)::text 
       END AS ParentPost,
       json_build_object('name', pu."name", 'profilePicture', pu."profilePicture") as parentPostAuthor,
-      COALESCE(c.commentCount, 0) AS commentCount,
-      COALESCE(s.sharedCount, 0) AS sharedCount -- Include the sharedCount for each post
+      CASE WHEN NOT EXISTS (
+        SELECT *
+        FROM public."Activity" a
+        WHERE a.author = ${req.user.user_id} AND a.type IN ('like', 'haha', 'love', 'sad', 'poop') AND a.post = p.id
+      ) THEN true ELSE false END AS noReaction,
+      (SELECT COUNT(*) FROM public."Activity" WHERE post = p.id AND type = 'like') AS likeCount,
+      (SELECT COUNT(*) FROM public."Activity" WHERE post = p.id AND type = 'haha') AS hahaCount,
+      (SELECT COUNT(*) FROM public."Activity" WHERE post = p.id AND type = 'sad') AS sadCount,
+      (SELECT COUNT(*) FROM public."Activity" WHERE post = p.id AND type = 'love') AS loveCount,
+      (SELECT COUNT(*) FROM public."Activity" WHERE post = p.id AND type = 'poop') AS poopCount,
+      COALESCE(s.sharedCount, 0) AS sharedCount,
+      COALESCE(c.commentCount, 0) AS commentCount -- Include the commentCount for each post
     FROM public."Posts" p
-    LEFT JOIN (
-      SELECT post,
-             COUNT(*) FILTER (WHERE type = 'like') AS likeCount,
-             COUNT(*) FILTER (WHERE type = 'haha') AS hahaCount,
-             COUNT(*) FILTER (WHERE type = 'sad') AS sadCount,
-             COUNT(*) FILTER (WHERE type = 'love') AS loveCount,
-             COUNT(*) FILTER (WHERE type = 'poop') AS poopCount
-      FROM public."Activity"
-      GROUP BY post
-    ) r ON r.post = p.id
     LEFT JOIN (
       SELECT post, STRING_AGG("tagName", ',') AS tagNames
       FROM public."Tags"
       GROUP BY "post"
     ) t ON p.id = t.post
-    LEFT JOIN public."Activity" a ON p.id = a.post AND a.author = ${req.user.user_id}
-    LEFT JOIN public."User" u ON p.author = u.id
+    JOIN public."User" u ON p.author = u.id
     LEFT JOIN public."Posts" pp ON pp.id = p.shared
     LEFT JOIN public."User" pu ON pp.author = pu.id
-    LEFT JOIN (
-      SELECT post_id, COUNT(*) as commentCount -- Subquery to get the comment count for each post
-      FROM public."Comment"
-      GROUP BY post_id
-    ) c ON p.id = c.post_id
     LEFT JOIN (
       SELECT shared, COUNT(*) as sharedCount -- Subquery to get the shared count for each post
       FROM public."Posts"
       GROUP BY shared
     ) s ON p.id = s.shared
-    WHERE EXISTS (
-      SELECT 1
-      FROM public."Friends" f
-      WHERE f.sender = ${req.user.user_id} AND f.reciever = p.author
-    ) AND p.deleted = false
+    LEFT JOIN (
+      SELECT post_id, COUNT(*) as commentCount -- Subquery to get the comment count for each post
+      FROM public."Comment"
+      GROUP BY post_id
+    ) c ON p.id = c.post_id
+    WHERE p.deleted = false
     ORDER BY p."createdAt" DESC;
-  `;
-  res.send(JSON.stringify(posts));
+`;
+
+
+  return posts;
+}
+
+async function getPost(req, res, prisma) {
+  console.log("get post for", req.user.user_id);
+  // recommandedPosts =await getRecommandedPost(req,prisma);
+  genericPost = await getGenericPost(req,prisma);
+
+
+  res.send(JSON.stringify(genericPost));
 }
 
 
